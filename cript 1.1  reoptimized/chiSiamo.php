8b2d41439b46a31d660941593feff9b9fb9eebf9
Francesco Di Patti <87224474+beckdregsta2@users.noreply.github.com>
2022-05-16 22:49:22 +0200

    Add files via upload

---
+++ cript 1.1  reoptimized/chiSiamo.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel= "stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet"> 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>Chi Siamo</title>
+    </head>
+
+    <body class="chiSiamo">
+        <nav>
+            <ul>
+                <div class="animazioneNav">
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="chiSiamo.php">CHI SIAMO</a></li>
+                    <li class="algoritmi"><a href="#">ALGORITMI</a>
+                </div>
+                        <div class="altriAlgoritmi">
+                            <ul>
+                                <li><a href="#">RSA</a></li>
+
+                                <li><a href="#">IDEA</a></li>
+
+                                <li><a href="#">DES</a></li>
+                            </ul>
+                        </div>
+                    </li>
+            </ul>
+        </nav>
+
+        <div class="container">
+            <div class="chiSiamo-info">       
+                <h1>LA NOSTRA STORIA</h1>
+                <p>pagina in creazione /////qui relazione GPOI 
+                    <br>
+
+                </p>
+            </div>
+       
+            
+        </div>     
+    </body>
+</html>
